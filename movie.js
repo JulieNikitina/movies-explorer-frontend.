6a399cec3bbbd5677f696e1e80d7b2c8ea5fae89
Julie <jq.nikitina@gmail.com>
2022-02-06 21:42:15 +0700

    All API for local use

---
+++ movie.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  director: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 4,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 100,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    maxlength: 30,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
